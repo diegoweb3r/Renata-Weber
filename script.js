be4bd954634159094d6e21a5a6e0a2e7be1cd9fd
Diego Felipe Weber <diegofelipeweber@hotmail.com>
2026-06-25 13:31:05 -0300

    Melhorias no nav responsivo

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const menuToggle = document.querySelector(".toggleMenu");
 const menu = document.getElementById("menu");
+const nav = document.getElementById("nav");
+
 
 menuToggle.addEventListener("click", () => {
   menu.classList.toggle("active");
